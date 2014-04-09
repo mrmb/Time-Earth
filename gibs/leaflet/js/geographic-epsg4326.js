526b77aca254758b3b8eeed340ac561d972b727f
Cavpollo <cavpollo@gmail.com>
2014-04-09 13:03:05 -0600

    Um... agregando la carpeta gibs que trae ejemplo de como jalar imagenes pero no ayuda en mucho...

---
+++ gibs/leaflet/js/geographic-epsg4326.js
@@ -0,0 +1,87 @@
+/**
+ * GIBS Web Examples
+ *
+ * Copyright 2013 - 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+window.onload = function() {
+
+    var EPSG4326 = new L.Proj.CRS(
+        "EPSG:4326",
+        "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", {
+            origin: [-180, 90],//14.6456172, -90.3157399],//
+            resolutions: [
+                0.5625,
+                0.28125,
+                0.140625,
+                0.0703125,
+                0.03515625,
+                0.017578125,
+                0.0087890625,
+                0.00439453125,
+                0.002197265625,
+				0.0010986328125,
+				0.00054931640625,
+				0.000274658203125,
+            ]
+        }
+    );
+
+    var map = L.map("map", {
+        center: [0, 0],
+        zoom: 4,
+        maxBounds: [
+            [-120, -220],
+            [120, 220]
+        ],
+        crs: EPSG4326
+    });
+
+    var template =
+        "https://map1{s}.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi" +
+        "?SERVICE=WMTS" +
+        "&REQUEST=GetTile" +
+        "&VERSION=1.0.0" +
+        "&LAYER={layer}" +
+        "&STYLE=" +
+        "&TILEMATRIXSET={tileMatrixSet}" +
+        "&TILEMATRIX={z}" +
+        "&TILEROW={y}" +
+        "&TILECOL={x}" +
+        "&FORMAT={format}" +
+        "&TIME={time}";
+
+    var layer = L.tileLayer(template, {
+        layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
+        tileMatrixSet: "EPSG4326_250m",
+        format: "image%2Fjpeg",
+        time: "2013-11-04",
+        tileSize: 512,
+        subdomains: "abc",
+        noWrap: true,
+        continuousWorld: true,
+        attribution:
+            "<a href='https://earthdata.nasa.gov/gibs'>" +
+            "NASA EOSDIS GIBS</a>&nbsp;&nbsp;&nbsp;" +
+            "<a href='https://github.com/nasa-gibs/web-examples/blob/master/leaflet/js/geographic-epsg4326.js'>" +
+            "View Source" +
+            "</a>"
+    });
+
+    map.addLayer(layer);
+};
+
