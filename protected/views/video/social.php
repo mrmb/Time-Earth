390a780b8636f500d22401be94199d6ec326a472
Cavpollo <cavpollo@gmail.com>
2014-04-13 11:33:28 -0600

    generate .mp4 video for analyze

--- protected/views/video/social.php
+++ protected/views/video/social.php
@@ -1,5 +1,49 @@
+<html>
+<head>
 <script type="text/javascript">
+	function analyze(folder, format){
+		$.ajax({
+			type: "GET",
+			url: "/index.php?r=video/AnalyzeVideo",
+			data: ( {
+				'dir': folder,
+				'format': format
+			} ) ,
+			cache: false,
+			dataType: "json",
+			success: function(data){
+				//$("#my_rand_image").attr("src", "videos/" + folder + "/" + folder + ".gif");
+				//window.location = "index.php?r=video/social&name=" +  folder + "&title=" + title + "&format=" + format;
+				alert('DONE: ' + data.result);
+			 } 
+		});
+	}
+	
+	function analyzeVideo(){
+		var url = window.location.search.substring(1);
+		url = url.substring(url.indexOf("?") + 1);
 
+		var query_string = {};
+		var vars = url.split("&");
+		
+		for (var i=0;i<vars.length;i++) {
+			var pair = vars[i].split("=");
+			
+			// If first entry with this name
+			if (typeof query_string[pair[0]] === "undefined") {
+				query_string[pair[0]] = pair[1];
+				// If second entry with this name
+			} else if (typeof query_string[pair[0]] === "string") {
+				var arr = [ query_string[pair[0]], pair[1] ];
+				query_string[pair[0]] = arr;
+				// If third or later entry with this name
+			} else {
+				query_string[pair[0]].push(pair[1]);
+			}
+		}
+		
+ 		analyze(query_string.name, query_string.format);
+	}
 
 	function parseURL(url){
 			
@@ -29,6 +73,8 @@
 		image_string = '<img src="' + '<?php Yii::app()->request->baseUrl; ?>' + "/videos/" +query_string.name + '/' +query_string.name + '.gif" >'  ;
 		$("#video").html(image_string);
 		$("#title").html(query_string.title.split('%20').join(' '));
+		
+		//document.write('<link rel=\'image_src\' href=\'http://cavpollos.com/Pics/Cortos/Corto0161.png\' / >'); DA ERROR =(
 	}
 
 	$(function() {
@@ -59,14 +105,21 @@
 	}
 
 </style>
+</head>
+<body>
 <br />
 <div id="title" style="position: relative; top: 10px; text-align:center;"></div>
 <br />
 <div id="video" style="position: relative; top: 10px;"></div>
-
+<br />
+<ul class="grid-nav-option">
+	<li><a href="#" onclick="analyzeVideo()">Analize Video</a></li>
+</ul>
+<br />
 <!-- Socialize -->
 <div id="social-area" class="page" style="position: relative; top: 0; left:36%">
 	
+
     	
             <div id="media" class="span12">
                 <!-- AddThis Button BEGIN -->
@@ -88,9 +141,5 @@
 </div>
 <!-- End Socialize -->
 
-
-
-
-
-
-
+</body>
+</html>
